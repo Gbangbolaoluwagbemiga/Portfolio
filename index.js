1d27000b838bafac78bca8e8eb2b67601d3909d3
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-03-22 14:36:03 +0100

    to be continued

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// Sticky navigation: Intersection Observer API
+
+const header = document.querySelector('.header');
+const topHeader = document.querySelector('.top--header');
+const navHeight = topHeader.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) topHeader.classList.add('sticky');
+  else topHeader.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+// Text animation
 const options = {
   strings: ['Oluwagbemiga', 'A Frontend Developer'],
   typeSpeed: 40,
