43a7d3358cc8fb75b0f7c13879f5deaddd6134ce
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-03-23 14:45:31 +0100

    Hamsburger successfully implemented

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const loader = document.querySelector('.loader');
 const header = document.querySelector('.header');
 const topHeader = document.querySelector('.top--header');
 const hamsburger = document.querySelector('.ham--icon');
+const cancelOpt = document.querySelector('.cancel--icon');
 const listItems = document.querySelector('ul');
 
 let clicked = true;
@@ -12,6 +13,13 @@ let clicked = true;
 //   loader.classList.add('hidden');
 //   container.classList.remove('hidden');
 // }, 5000);
+
+// Helper functions
+const hamsburg = function (hamsDisplay, cancelDisplay, listDisplay) {
+  hamsburger.style.display = hamsDisplay;
+  cancelOpt.style.display = cancelDisplay;
+  listItems.style.display = listDisplay;
+};
 
 // Sticky navigation: Intersection Observer API
 const navHeight = topHeader.getBoundingClientRect().height;
@@ -31,7 +39,7 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
-// Hamburger functionality
+// Hamsburger functionality
 function handleViewportChange() {
   const mediaQueryList = window.matchMedia('(max-width: 800px)');
 
@@ -39,14 +47,24 @@ function handleViewportChange() {
     listItems.style.display = 'block';
   }
 }
+
+// Hamsburger event handlers
 hamsburger.addEventListener('click', function () {
   handleViewportChange();
   if (clicked) {
-    listItems.style.display = 'block';
-  } else {
-    listItems.style.display = 'none';
+    hamsburg('none', 'block', 'block');
+  }
+  if (!clicked) {
+    return (
+      (hamsburger.style.display = 'none'), (cancelOpt.style.display = 'block')
+    );
   }
   clicked = !clicked;
+});
+
+// Dismissal: Hamsburger
+cancelOpt.addEventListener('click', function () {
+  hamsburg('block', 'none', 'none');
 });
 
 // Text animation
