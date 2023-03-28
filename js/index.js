71dc56c46dcbdb4ce77f4feb1557a5dace1b7269
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-03-28 12:44:22 +0100

    section contact getting done

--- js/index.js
+++ js/index.js
@@ -156,7 +156,30 @@ allSections.forEach(function (section) {
 slider.slideProjects();
 
 // section mail
-// const mailMe = document.querySelector('.mail-me');
-// mailMe.addEventListener('submit', function (e) {
-//   e.preventDefault();
-// });
+
+// modal
+const modal = document.querySelector('.contact-modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnOpenModal = document.querySelector('.send-mail');
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnOpenModal.addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
