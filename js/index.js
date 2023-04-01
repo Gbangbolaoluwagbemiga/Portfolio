1a812a39297a350671a62c42ebf2f92f76d339b4
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-04-01 02:06:27 +0100

    mail section

--- js/index.js
+++ js/index.js
@@ -224,34 +224,34 @@ allSections.forEach(function (section) {
 // project slider
 slider.slideProjects();
 
+const arrowUp = document.querySelector('.back-up');
 // section mail
 
 // modal
-const modal = document.querySelector('.contact-modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnOpenModal = document.querySelector('.send-mail');
-const arrowUp = document.querySelector('.back-up');
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-  arrowUp.classList.add('hidden');
-};
+// const modal = document.querySelector('.contact-modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.btn--close-modal');
+// const btnOpenModal = document.querySelector('.send-mail');
+// const openModal = function (e) {
+//   e.preventDefault();
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+//   arrowUp.classList.add('hidden');
+// };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-  arrowUp.classList.remove('hidden');
-};
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+//   arrowUp.classList.remove('hidden');
+// };
 
-btnOpenModal.addEventListener('click', openModal);
+// btnOpenModal.addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
