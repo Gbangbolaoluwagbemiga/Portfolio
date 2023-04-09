357c256d37838dccb23bcde2d9b3da37aa2c1a05
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-04-09 02:26:47 +0100

    menu fade animation added to the header

--- js/index.js
+++ js/index.js
@@ -50,6 +50,22 @@ function toggleDarkMode() {
   // });
 }
 
+// menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+header.addEventListener('mouseover', handleHover.bind(0.5));
+header.addEventListener('mouseout', handleHover.bind(1));
+
 // Sticky navigation: Intersection Observer API
 const arrowUp = document.querySelector('.back-up');
 const navHeight = topHeader.getBoundingClientRect().height;
