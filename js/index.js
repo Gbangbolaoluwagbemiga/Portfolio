a93034a88e1c9b341147c9c3f0c9a61afaf5c0e7
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-04-09 02:27:30 +0100

    signing out

--- js/index.js
+++ js/index.js
@@ -55,7 +55,7 @@ const handleHover = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img');
+    // const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
       if (el !== link) el.style.opacity = this;
