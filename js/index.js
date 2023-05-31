23233bea9d71ce1a8a6c30f71c05102075ced167
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-05-31 14:43:48 +0100

    tech stack updated

--- js/index.js
+++ js/index.js
@@ -192,17 +192,17 @@ chooseView.addEventListener('click', function () {
     : (chooseViewText.innerText = 'see less');
   if (mediaQueryList.matches) {
     if (!readMoreText.classList.contains('hidden') && mediaQueryList.matches) {
-      subHeader.style.height = '135vh';
+      subHeader.style.height = '134vh';
     } else {
-      subHeader.style.height = '95vh';
+      subHeader.style.height = '94vh';
     }
   }
   if (!mediaQueryList.matches) {
     if (!readMoreText.classList.contains('hidden') && !mediaQueryList.matches) {
-      subHeader.style.height = '38rem';
+      subHeader.style.height = '37rem';
       myProfilePics.classList.add('middle');
     } else {
-      subHeader.style.height = '35rem';
+      subHeader.style.height = '34rem';
       myProfilePics.classList.remove('middle');
     }
   }
@@ -256,4 +256,47 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
+});
+
+// Tech stack section
+document.addEventListener('DOMContentLoaded', function () {
+  const techItems = Array.from(document.querySelectorAll('.tech-item'));
+  const connector = document.querySelector('.tech-connector');
+
+  const angle = (2 * Math.PI) / techItems.length;
+
+  techItems.forEach((item, index) => {
+    const radius = 150; // Adjust the radius as needed
+    const x = Math.cos(angle * index) * radius;
+    const y = Math.sin(angle * index) * radius;
+
+    gsap.set(item, {x, y}); // Set initial position of each item
+
+    const line = document.createElement('div');
+    line.className = 'connector-line';
+    line.style.width = `${Math.sqrt(x * x + y * y)}px`;
+    line.style.transform = `rotate(${Math.atan2(y, x)}rad)`;
+    connector.appendChild(line);
+  });
+
+  function rotateItems() {
+    techItems.forEach((item, index) => {
+      const newIndex = (index + 1) % techItems.length; // Get the new index after rotation
+      const newX = gsap.getProperty(techItems[newIndex], 'x');
+      const newY = gsap.getProperty(techItems[newIndex], 'y');
+
+      gsap.to(item, {
+        x: newX,
+        y: newY,
+        duration: 2,
+        ease: 'power1.inOut',
+      });
+    });
+  }
+
+  // Start rotating the items
+  rotateItems();
+
+  // Rotate the items every 2 seconds
+  setInterval(rotateItems, 2000);
 });
